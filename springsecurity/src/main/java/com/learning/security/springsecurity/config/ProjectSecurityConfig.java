253419fe3745fab1f27d1fe6a9583cbcb91bd4d6
Balram Verma <vermabalram663@gmail.com>
2025-03-07 12:05:57 +0530

    Userdetails using inmemoryDatails

--- springsecurity/src/main/java/com/learning/security/springsecurity/config/ProjectSecurityConfig.java
+++ springsecurity/src/main/java/com/learning/security/springsecurity/config/ProjectSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,5 +24,12 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user= User.withUsername("user").password("{noop}root").roles("USER").build();
+        UserDetails admin= User.withUsername("admin").password("{noop}root").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
