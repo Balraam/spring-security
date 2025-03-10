cf063fa029562f23ed082ad320fb2631c65f892b
Balram Verma <vermabalram663@gmail.com>
2025-03-10 21:31:17 +0530

    Custum userDetails service

--- springsecurity/src/main/java/com/learning/security/springsecurity/config/ProjectSecurityConfig.java
+++ springsecurity/src/main/java/com/learning/security/springsecurity/config/ProjectSecurityConfig.java
@@ -1,17 +1,11 @@
 package com.learning.security.springsecurity.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -23,19 +17,27 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myCards","/myAccount","/myLoans","/myBalance").authenticated()
-                .requestMatchers("/myContact","/myNotice","/error").permitAll());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myCards","/myAccount","/myLoans","/myBalance").authenticated()
+                .requestMatchers("/myContact","/myNotice","/error","/registerUser").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
-    @Bean
+    // inmemory user
+   /* @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user= User.withUsername("user").password("{noop}BV@7693").roles("USER").build();
         UserDetails admin= User.withUsername("admin").password("{bcrypt}$2a$12$hX8V4j3Gt/iffYR6Rv7BBufP2CjKz5RJhDx3xU73TKe6d2nJcd/Cq").roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user,admin);
-    }
+    }*/
+
+    //database
+   /* @Bean
+    public UserDetailsService userDetailsService(DataSource datasource) {
+               return new JdbcUserDetailsManager(datasource);
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- springsecurity/src/main/resources/sql/scripts.sql
+++ springsecurity/src/main/resources/sql/scripts.sql
@@ -1,3 +1,20 @@
 create table users(username varchar(50) not null primary key,password varchar(500) not null,enabled boolean not null);
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
+
+INSERT IGNORE INTO `users` VALUES ('user', '{noop}BV@7693', '1');
+INSERT IGNORE INTO `authorities` VALUES ('user', 'read');
+
+INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$hX8V4j3Gt/iffYR6Rv7BBufP2CjKz5RJhDx3xU73TKe6d2nJcd/Cq', '1');
+INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{noop}BV@7693', 'read');
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$hX8V4j3Gt/iffYR6Rv7BBufP2CjKz5RJhDx3xU73TKe6d2nJcd/Cq', 'admin');
